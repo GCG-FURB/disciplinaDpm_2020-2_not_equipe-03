60ccd30c341e30b1325f24ddb7f4465727327fd2
Carminati07 <maik.carminati@outlook.com>
2020-10-20 19:51:28 -0300

    Atividade 3C

---
+++ 3C/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MinhaApp());
+}
+
+class MinhaApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text('Hello World!!!'));
+  }
+}
