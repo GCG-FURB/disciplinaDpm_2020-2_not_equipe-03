28530de14a4233704b2500a0b7807c3e7ca1252e
Carminati07 <maik.carminati@outlook.com>
2020-12-08 20:16:49 -0300

    trabalho final

---
+++ trabalhoFinal_flutter/android/app/src/main/kotlin/com/example/trabalhoFinal_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.trabalhoFinal_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ trabalhoFinal_flutter/lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  LatLng _initialcameraposition =
+      LatLng(-26.935142691275004, -49.03644829548792);
+  GoogleMapController _controller;
+  Location _location = Location();
+
+  List<Circle> polygons = <Circle>[
+    new Circle(
+        circleId: null,
+        strokeColor: Colors.blue,
+        strokeWidth: 10,
+        fillColor: Colors.blue.withOpacity(0.1))
+  ];
+
+  void _onMapCreated(GoogleMapController _cntlr) {
+    _controller = _cntlr;
+    _location.onLocationChanged.listen((l) {
+      _controller.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(target: LatLng(l.latitude, l.longitude), zoom: 18),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Stack(
+          children: [
+            GoogleMap(
+              initialCameraPosition:
+                  CameraPosition(target: _initialcameraposition),
+              mapType: MapType.normal,
+              onMapCreated: _onMapCreated,
+              myLocationEnabled: true,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
